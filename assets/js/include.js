25038624dbfd9782ea9f6684f1221f8c0b804685
nicolaszc <nzakrzewicz@gmail.com>
2025-11-17 22:49:26 -0300

    include to change page

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,41 +1,38 @@
 // include.js
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-  if (window.location.href === "https://ddvr.cl" || window.location.href === "http://localhost:3000/") {
-    console.log("Index");
   
-    Promise.all([includeHTML("components/header-no-nav.html", "header"),
-      includeHTML("components/footer.html", "footer")
-    ]).then(() => {
-      console.log("Componentes cargados ✅");
-      document.dispatchEvent(new Event("componentsLoaded"));
-    });
-  }
-
-  if (window.location.href === "https://ddvr.cl/home.html" || window.location.href === "http://localhost:3000/home.html") {
-    console.log("Home");
+    console.log('Home');
   
     Promise.all([
-      includeHTML("components/header.html", "header"),
-      includeHTML("components/footer.html", "footer"),
-      includeHTML("components/fixed-menu.html", "fixed-menu"),
-      includeHTML("components/product-modal.html", "product-modal"),
-      includeHTML("components/contact-form.html", "contact-form")
+      includeHTML('components/header.html', 'header', 'include'),
+      includeHTML('components/catalogo.html', 'main', 'include'),
+      includeHTML('components/hero.html', 'main', 'insert'),
+      includeHTML('components/footer.html', 'footer', 'include'),
+      includeHTML('components/fixed-menu.html', 'fixed-menu', 'include'),
+      includeHTML('components/product-modal.html', 'product-modal', 'include'),
+      includeHTML('components/contact-form.html', 'contact-form', 'include')
     ]).then(() => {
-      console.log("Componentes cargados ✅");
-      document.dispatchEvent(new Event("componentsLoaded"));
+      console.log('Componentes cargados ✅');
+      document.dispatchEvent(new Event('componentsLoaded'));
     });
-  }
+  
 });
 
-function includeHTML(file, elementId) {
+function includeHTML(file, elementId, method) {
+  let includeElementTarget = document.getElementById(elementId);
   return fetch(file)
     .then(response => {
       if (!response.ok) throw new Error(`No se pudo cargar ${file}`);
       return response.text();
     })
     .then(data => {
-      document.getElementById(elementId).innerHTML = data;
+      if(method == 'include'){
+        includeElementTarget.innerHTML = data;
+      }
+      if(method == 'insert'){
+        includeElementTarget.insertAdjacentHTML('afterbegin', data);
+      }
     });
 }
 
